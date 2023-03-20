5431e3a1dfab529bd2039e4fb676c5e59128afa3
jjones <jimmy.jones@tutanota.com>
2023-03-20 16:17:57 -0600

    fix import of forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -9,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgModule,
+    FormsModule
+    ,
   ],
   providers: [],
   bootstrap: [AppComponent]
